0a58c486fcd5034f8e1f9de43d887cb69631c2e4
SanderBackx1 <sander.backx123@gmail.com>
2021-06-18 12:54:42 +0200

    temp fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div v-if="!$fetchState.pending">
+      <ul>
+        <li v-for="user in users.results" :key="user.login.uuid">
+          {{ user.name.first }} {{ user.name.last }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      Loading...
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  async fetch() {
+    const response = await fetch("https://randomuser.me/api/?results=5");
+    this.users = await response.json();
+  }
+};
 </script>
 
 <style></style>
