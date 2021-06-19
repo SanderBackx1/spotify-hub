590e1ad9923c7dc01b5566b780ded504a7aa57ec
SanderBackx1 <sander.backx123@gmail.com>
2021-06-19 12:21:11 +0200

    style setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,49 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: [],
+  purge: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    },
+    colors: {
+      white: colors.white,
+      black: colors.black,
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      green: {
+        DEFAUT: "#1DB954"
+      },
+      gradient1: { DEFAULT: `#5B5773` }
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"]
+    },
+    extend: {
+      spacing: {
+        "128": "32rem",
+        "144": "36rem"
+      },
+      borderRadius: {
+        "4xl": "2rem"
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
