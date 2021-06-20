8bf7cce5cf30890d92b12526eac139240f2f3e3c
SanderBackx1 <sander.backx123@gmail.com>
2021-06-20 13:40:43 +0200

    layout + display name and id

--- components/chat/Input.vue
+++ components/chat/Input.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="input-wrapper">
-    <button @click="writeMessage">Send</button>
-    <input type="text" v-model="msg" />
+  <div class="input-wrapper bg-black text-white">
+    <div class="content-wrapper px-4 py-8 flex items-center">
+      <button
+        class="bg-green rounded-full px-4 py-2 text-sm mr-4"
+        @click="writeMessage"
+      >
+        Send
+      </button>
+      <input
+        class="bg-blackLight rounded-lg p-2 w-48"
+        type="text"
+        v-model="msg"
+        @keypress.enter="writeMessage"
+      />
+    </div>
   </div>
 </template>
 
@@ -20,7 +32,8 @@ export default {
         .push()
         .set({
           msg: this.msg,
-          from: this.$store.getters.getUser().id,
+          from: this.$store.getters.getUser().display_name,
+          fromId: this.$store.getters.getUser().id,
           sentAt: Date.now()
         });
 
