ab5bfa962f2d502905055e8cd9161918d3f6f2fc
SanderBackx1 <sander.backx123@gmail.com>
2021-06-20 12:00:50 +0200

    track display

---
+++ components/host/CurrentTrackDisplay.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full flex justify-center items-center">
+    <TrackMediumDisplay />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ components/track/Progressbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="relative pt-1">
+    <div
+      class="  rounded-lg overflow-hidden h-2 mb-4 text-xs flex rounded bg-blackLight"
+    >
+      <div
+        :style="`width:${calculateProgress(state)}%`"
+        class="rounded-lg shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gray-400"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["state"],
+
+  methods: {
+    calculateProgress(state) {
+      if (state.progress_ms && state.item.duration_ms) {
+        return (state.progress_ms / state.item?.duration_ms) * 100;
+      }
+      return 0;
+    }
+  }
+};
+</script>
+
+<style></style>
